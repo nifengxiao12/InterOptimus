aa3329e367dd02a06b0040fdac49d9dac4b36aa9
Jason Xie <393106066@qq.com>
2024-10-29 00:00:33 +0800

    1

--- InterOptimus/setup.py
+++ InterOptimus/setup.py
@@ -8,18 +8,19 @@ setup(
     description="High througput simulation making crystalline interfaces",   # 简短描述
     long_description=open("README.md").read(),  # 从README中读取长描述
     long_description_content_type="text/markdown",  # 描述文件格式
-    url="https://github.com/yourusername/your_project",  # 项目主页
+    url="https://github.com/HouGroup/InterOptimus/",  # 项目主页
     packages=find_packages(),                # 自动查找项目中的包
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",  # 或其他许可证
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",                 # Python版本要求
+    python_requires=">=3.12",                 # Python版本要求
     install_requires=[
         # 在这里列出项目依赖，比如：
         "requests",
         "numpy",
+        "interface_master"
     ],
     entry_points={
         "console_scripts": [
